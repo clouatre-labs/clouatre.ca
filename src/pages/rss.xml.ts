1e3bff6b81e872870693cbcd1ac2c7c5b39810df
Hugues Clouâtre <hugues@linux.com>
2026-01-14 22:59:37 -0500

    feat: add full content to RSS feed with markdown rendering
    - Add markdown-it and sanitize-html dependencies
    - Parse post body markdown to HTML in RSS items
    - Include author field from SITE config
    - Sanitize HTML output with img tag support for post images
    
    Signed-off-by: Hugues Clouâtre <hugues@linux.com>

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,12 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 import { SITE } from "@/config";
 import { getPath } from "@/utils/getPath";
 import getSortedPosts from "@/utils/getSortedPosts";
+
+const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection("blog");
@@ -11,11 +15,15 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data, id, filePath }) => ({
+    items: sortedPosts.map(({ data, id, filePath, body }) => ({
       link: getPath(id, filePath),
       title: data.title,
       description: data.description,
       pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+      author: SITE.author,
+      content: sanitizeHtml(parser.render(body ?? ""), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
   });
 }
